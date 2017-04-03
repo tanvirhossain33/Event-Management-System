8faaa50547b13ceb18122d89fa6c657d9624a9db
tanvir <hossaint33@gmail.com>
2017-04-03 18:15:34 +0600

    add event-detail component

---
+++ app/events/event-detail.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core'
+
+@Component({
+    templateUrl: 'app/events/event-detail.component.html'
+})
+
+export class EventDetailComponent{
+    pageTitle: string = 'Event Detail';
+}
